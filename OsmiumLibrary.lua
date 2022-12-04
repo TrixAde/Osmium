7bc791f443d59f656409ef32dc3c96deebfdc558
Trix <80095537+TrixAde@users.noreply.github.com>
2022-12-04 14:51:01 +0100

    Update OsmiumLibrary.lua

--- OsmiumLibrary.lua
+++ OsmiumLibrary.lua
@@ -1057,7 +1057,7 @@ local library = (function()
 							searchbar.Visible = true
 							dropdownSelectedLabel.Visible = false
 						else
-							dropdown:TweenSize(UDim2.new(0, 470, 0, 40), "Out", "Quint", 0.2)
+							dropdown:TweenSize(UDim2.new(0, 438, 0, 35), "Out", "Quint", 0.2)
 							dropdown.TextButton:TweenPosition(UDim2.new(0, 0, 1, 0), "Out", "Quint", 0.2)
 							scrollingframe.Visible = false
 							stroke.Enabled = false
@@ -1134,4 +1134,40 @@ local library = (function()
 
 end)()
 
-return library
+local window = library:CreateWindow("Osmium UI Library")
+
+local test = window:CreateTab("Main")
+local info = window:CreateTab("Info")
+local cred = window:CreateTab("Credits")
+
+local dropdown = test:CreateDropdown("DropDown Exemple",{"Nami","Robin","Yamato"},function(val)
+	print(val)
+end)
+
+local label = test:CreateLabel("This is a Title","this is an exemple of description")
+
+local sld = test:CreateSlider("Slider Exemple",-100,100,function(arg)
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = arg
+end)
+
+test:CreateTextbox("TextBox Exemple", function(value)
+    print("Value = ", value)
+end, "Write Here")
+
+local toggle = test:CreateToggle("Toggle Exemple",false,function()
+    
+end)
+
+local batp = test:CreateButton("Button Exemple", function()
+    print("c")
+end)
+
+local label = info:CreateLabel("KeyBind :","KeyBind to Close/Open the Gui Is 'Left Alt'")
+
+local label = cred:CreateLabel("Interface :","Made by Trix#2794")
+local label = cred:CreateLabel("Interface Scripts :","Made by Trix#2794")
+local label = cred:CreateLabel("Scripting :","by Trix#2794 / JulMan#1234")
+local batp = cred:CreateButton("Copy Discord Server Link", function()
+    setclipboard("discord.gg/TT3y4gkJtq")
+end)
+
